f7c64468fa0a4113150ba2a38c761e0e269a4877
VU Nhat Tuan <nvu1@mantu.com>
2022-10-09 11:26:11 +1100

    remove data sections

--- Get_CurrentTime.sh
+++ Get_CurrentTime.sh
@@ -5,8 +5,3 @@ function Get_CurrentTime()
     local CURRENT_TIME=$(date +$1)
     echo $CURRENT_TIME
 }
-
-# TODO: we need to treat this time format as a data section, next step is to move this to the implementation place
-TIME_FORMAT="%H%M%S%d%m%Y"
-result=$(Get_CurrentTime $TIME_FORMAT)
-echo $result
