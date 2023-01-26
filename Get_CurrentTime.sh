75c538dff9f32c5f799c3e192d5c7eed33e7f06c
tuanvu0606 <tuanvu0695@gmail.com>
2023-01-26 18:06:54 +1100

    Update Get_CurrentTime.sh
    Added example and description

--- Get_CurrentTime.sh
+++ Get_CurrentTime.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script does getting time with a format passed as the second argument
+# Example: Get_CurrentTime "%y%m%d%H%M%S"
+
 function Get_CurrentTime()
 {
     local CURRENT_TIME=$(date +$1)
